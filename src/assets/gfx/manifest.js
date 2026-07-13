9de99abe8c444238f7681d459ba68ee977fa074f
swatdev <152759204+swatntj@users.noreply.github.com>
2026-07-13 14:36:07 -0300

    Update GFX portfolio site

---
+++ src/assets/gfx/manifest.js
@@ -0,0 +1,35 @@
+﻿window.SWAT_GFX_ASSETS = [
+    {
+        "src":  "src/assets/gfx/brand-icon-01.png",
+        "title":  "Brand Icon 01"
+    },
+    {
+        "src":  "src/assets/gfx/brand-icon-02.png",
+        "title":  "Brand Icon 02"
+    },
+    {
+        "src":  "src/assets/gfx/brand-icon-03.png",
+        "title":  "Brand Icon 03"
+    },
+    {
+        "src":  "src/assets/gfx/brand-icon-04.png",
+        "title":  "Brand Icon 04"
+    },
+    {
+        "src":  "src/assets/gfx/gfx-banner-01.png",
+        "title":  "Gfx Banner 01"
+    },
+    {
+        "src":  "src/assets/gfx/gfx-banner-02.png",
+        "title":  "Gfx Banner 02"
+    },
+    {
+        "src":  "src/assets/gfx/gfx-banner-03.png",
+        "title":  "Gfx Banner 03"
+    },
+    {
+        "src":  "src/assets/gfx/gfx-banner-04.png",
+        "title":  "Gfx Banner 04"
+    }
+];
+
